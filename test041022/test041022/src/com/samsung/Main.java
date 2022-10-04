7e0a1f373418e29c7551b8112ab1cd0850add8a5
Vesan1a <114503620+Vesan1a@users.noreply.github.com>
2022-10-04 20:51:52 +0300

    cw041022

---
+++ test041022/test041022/src/com/samsung/Main.java
@@ -0,0 +1,17 @@
+package com.samsung;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        double x = scanner.nextDouble();
+        double y = scanner.nextDouble();
+
+        String res = (y > 0 && x * x = y * y > 4 && x < 2 && y < x) ? "YES" : "NO"
+
+
+    }
+}
